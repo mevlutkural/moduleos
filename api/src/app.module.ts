8bed5d4e10feb6807c5eaaa47c3a7828e8c147f3
Mevlüt Kural <mevlutkural35@gmail.com>
2026-01-07 00:23:58 +0300

    feat: Add API rate limiting with configurable throttler and externalize i18n module configuration.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,29 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
-import { join } from 'node:path';
 import { ExceptionHandlerModule } from './shared/exception-handler/exception-handler.module';
 import { APP_FILTER } from '@nestjs/core';
 import { GlobalExceptionFilter } from './shared/filters/global-exception.filter';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { dbConfig, DbConfigService } from './shared/config/db.config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import {
+  throttlerConfig,
+  ThrottlerConfigService,
+} from './shared/config/throttler.config';
+import { I18nModule } from 'nestjs-i18n';
+import { i18nConfig, I18nConfigService } from './shared/config/i18n.config';
 
 @Module({
   imports: [
-    I18nModule.forRoot({
-      fallbackLanguage: 'en',
-      loaderOptions: {
-        path: join(__dirname, './lib/i18n'),
-        watch: true,
-      },
-      resolvers: [AcceptLanguageResolver],
-    }),
-    ExceptionHandlerModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule.forFeature(dbConfig)],
       useClass: DbConfigService,
+    }),
+    I18nModule.forRootAsync({
+      imports: [ConfigModule.forFeature(i18nConfig)],
+      useClass: I18nConfigService,
+    }),
+    ExceptionHandlerModule,
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule.forFeature(throttlerConfig)],
+      useClass: ThrottlerConfigService,
     }),
   ],
   controllers: [AppController],
